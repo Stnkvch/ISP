65634a04643d974a6313b11fca043e29d5b074a7
Denurex <79199000+Denurex@users.noreply.github.com>
2021-05-17 00:26:48 +0300

    Add files via upload

---
+++ Lr6/Lr5/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Lr5
+{
+    interface ISport
+    {
+        void Training();
+    }
+    interface IInfo
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+    }
+
+    class Program
+    {
+        static int Switcher;
+        static void Main(string[] args)
+        {         
+            bool bolean = true;
+            bool check = false;
+            Console.WriteLine("Choose:\n1-Football\n2-Bascetball\n3-Swiming");
+            do
+            {
+                while (!check)
+                {
+                    try
+                    {
+                        Switcher = Convert.ToInt32(Console.ReadLine());
+                        check = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Вы ввели неверное значение");
+                    }
+                }
+                switch (Switcher)
+                {
+                    case 1:
+                        Football BobF = new Football();
+                        Sportsmen.FillInfo(BobF);
+                        BobF.AddLevel();
+                        do
+                        {
+                            Console.WriteLine("1-Throw\n2-training\n3-info");
+                            Switcher = Convert.ToInt32(Console.ReadLine());
+                            switch (Switcher)
+                            {
+                                case 1: BobF.Throw(); bolean = false; break;
+                                case 2: BobF.Training(); bolean = false; break;
+                                case 3: Sportsmen.ShowInfo(BobF); bolean = false; break;
+                                default: Console.WriteLine("Вы ввели неверное значение"); break;
+                            }
+                        } while (!bolean && BobF.Energy > 0);
+                        Sportsmen.ShowInfo(BobF);
+                        bolean = false; break;
+                    case 2:
+                        Bascet BobB = new Bascet();
+                        Sportsmen.FillInfo(BobB);
+                        BobB.AddLevel();
+                        do
+                        {
+                            Console.WriteLine("1-Throw\n2-training\n3-info");
+                            Switcher = Convert.ToInt32(Console.ReadLine());
+                            switch (Switcher)
+                            {
+                                case 1: BobB.Throw(); bolean = false; break;
+                                case 2: BobB.Training(); bolean = false; break;
+                                case 3: Sportsmen.ShowInfo(BobB); bolean = false; break;
+                                default: Console.WriteLine("Вы ввели неверное значение"); break;
+                            }
+                        } while (!bolean && BobB.Energy > 0);
+                        Sportsmen.ShowInfo(BobB);
+                        bolean = false; break;
+                    case 3:
+                        Swim BobS = new Swim();
+                        Sportsmen.FillInfo(BobS);
+                        BobS.AddLevel();
+                        BobS.Training();
+                        Sportsmen.ShowInfo(BobS);
+                        bolean = false; break;
+                    default: Console.WriteLine("Вы ввели неверное значение"); check = false; break;
+                }
+            } while (bolean);
+        }
+    }
+}
---
+++ Lr6/Lr5/Sportsmen.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lr5
+{
+ 
+
+    class Sportsmen : IInfo
+    {
+        enum level
+        {
+            low, strong, expert
+        }
+
+        private string LevelS;
+
+        public int Energy = 100;
+        private string name;
+        private int age;
+        private int weight;
+        private string club;
+        static bool check1 = false;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Club
+        {
+            get { return club; }
+            set { club = value; }
+        }
+
+        public int Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public static void FillInfo(Sportsmen Bob)
+        {
+            Console.WriteLine("Enter Name: ");
+            Bob.Name = Console.ReadLine();
+            while (!check1)
+            {
+                try
+                {
+                    Console.WriteLine("Enter Age: ");
+                    Bob.Age = Convert.ToInt32(Console.ReadLine());
+                    check1 = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Вы ввели неверное значение");
+                }
+            }
+            check1 = false;
+            while (!check1)
+            {
+                try
+                {
+                    Console.WriteLine("Enter Weight: ");
+                    Bob.Weight = Convert.ToInt32(Console.ReadLine());
+                    check1 = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Вы ввели неверное значение");
+                }
+            }
+
+            Console.WriteLine("Enter Club Name: ");
+            Bob.Club = Console.ReadLine();
+        }
+
+        public static void ShowInfo(Sportsmen Bob)
+        {
+            Console.WriteLine($"\nName: {Bob.Name}\nAge: {Bob.Age}\nWeight: {Bob.Weight}\nClub name: {Bob.Club}\nEnergy: {Bob.Energy}\nLevel: {Bob.LevelS}");
+        }
+
+        public void AddLevel()
+        {
+            Console.WriteLine("Enter Category\n0: low\n1: strong\n2: Expert");
+            int CategBuf = Convert.ToInt32(Console.ReadLine());
+            LevelS = ($"{Enum.GetName(typeof(level), CategBuf)}");
+        }
+    }
+}
